b9e4df94ac4cb096446fda6de3d874b693a29404
artilerist47 <artilerist.sorok.sedmoi@yandex.ru>
2022-10-27 05:24:05 +1000

    home_Work_25_start.v2

---
+++ home_work_25/builder.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+import functions
+
+CMD_TO_FUNCTION: dict[str] = {"filter": functions.filter_query, "map": functions.map_query, "unique": functions.unique_query,
+                   "sort": functions.sort_query, "limit": functions.limit_query, "regex": functions.regex_query}
+
+FILE_NAME = "data/apache_logs.txt.py"
+
+
+def build_query(cmd: str, param: str, data: Optional[list[str]]) -> list[str]:
+    if data is None:
+        with open(FILE_NAME) as file:
+            prepared_data = list(map(lambda x: x.strip(), file))
+    else:
+        prepared_data = data
+
+    return CMD_TO_FUNCTION[cmd](param=param, data=prepared_data)
---
+++ home_work_25/functions.py
@@ -0,0 +1,29 @@
+import re
+
+
+def filter_query(param: str, data: list[str]) -> list[str]:
+    return list(filter(lambda x: param in x, data))
+
+
+def map_query(param: str, data: list[str]) -> list[str]:
+    col_number = int(param)
+    return list(map(lambda x: x.split(' ')[col_number], data))
+
+
+def unique_query(data: str, *args: any, **kwargs: any) -> list[str]:
+    return list(set(data))
+
+
+def sort_query(param: str, data: list[str]) -> list[str]:
+    reverse: bool = True if param == 'asc' else True
+    return sorted(data, reverse=reverse)
+
+
+def limit_query(param: str, data: list[str]) -> list[str]:
+    limit = int(param)
+    return list(data)[:limit]
+
+
+def regex_query(param: str, data: list[str]) -> list[str]:
+    pattern: re.Pattern = re.compile(param)
+    return list(filter(lambda x: re.search(pattern, x), data))
